267afcaa97f11aa272bbc031dc7c1de8adf959e1
zeynepozer1 <102956807+zeynepozer1@users.noreply.github.com>
2022-04-08 02:21:59 +0300

    Update GitHubCalısması.java

--- src/test/java/GitHub/GitHubCalısması.java
+++ src/test/java/GitHub/GitHubCalısması.java
@@ -4,6 +4,8 @@ public class GitHubCalısması {
 
     public static void main (String[]args){
         System.out.println("GitHub Dersi basladı" );
+        
+        System.out.println("GitHub Dersinde basarılar" );
 
         ;
     }
